5c8e9e67c7b6fd2400139554175d46070f024d05
zhangxm <zhangxm@youpu.me>
2017-05-09 11:32:02 +0800

    接口地址修改

--- application/library/enum/System.php
+++ application/library/enum/System.php
@@ -4,8 +4,9 @@ class Enum_System {
 
     const RPC_REQUEST_PACKAGE = 'ia';
 
-    //    const SERVICE_API_DOMAIN = 'http://testapi.liheinfo.com';
-    const SERVICE_API_DOMAIN = 'http://api-dev.easyiservice.com';
+    const SERVICE_API_DOMAIN = 'http://testapi.liheinfo.com';
+
+    //    const SERVICE_API_DOMAIN = 'http://api-dev.easyiservice.com';
 
     public static function getServiceApiUrlByLink($url) {
         $url = strpos('http', $url) ? $url : self::SERVICE_API_DOMAIN . $url;
