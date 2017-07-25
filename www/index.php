b04f4a51ba7f330a65c9414ff28e677abff17c0e
zhangxm <zhangxm@youpu.me>
2017-07-25 10:56:14 +0800

    时区修改

--- www/index.php
+++ www/index.php
@@ -3,6 +3,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL ^ E_NOTICE);
 error_reporting(0);
 define('APPLICATION_PATH', dirname(__FILE__) . "/../");
+date_default_timezone_set('PRC');
 
 $application = new Yaf_Application(APPLICATION_PATH . "/env/application.ini");
 $application->bootstrap()->run();
